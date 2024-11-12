0c0fdaa4128c8741629a11b4c96fa9177434b416
Leo-Pellegrin <leo.pellegrin87@gmail.com>
2024-11-11 19:42:42 -0500

    Correction chemin deploy gh pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,7 +13,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rest-countries-api-with-color-theme-switcher-master",
+  base: "/countries-api",
   plugins: [
     VueRouter(),
     Layouts(),
